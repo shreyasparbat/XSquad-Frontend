a0b52714a02a1237d87eb12029a8af969cba1027
Myo <xxyu.2014@sis.smu.edu.sg>
2018-03-03 18:21:34 +0800

    added drawer navigation and editing the stack navigation

--- App.js
+++ App.js
@@ -1,7 +1,126 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+import { StyleSheet, Text, View, Button, ScrollView, Platform, Image, StatusBar } from 'react-native';
+import { Root, Icon, Container, Header, Content, Body } from 'native-base';
+import { StackNavigator, DrawerNavigator, DrawerItems } from 'react-navigation';
+
+import LoginScreen from './app/login/LoginScreen';
+import LoginWithEmailScreen from './app/login/LoginWithEmailScreen';
+import RegisterScreen from './app/login/RegisterScreen';
+
+import BasketSummaryScreen from './app/cart/BasketSummaryScreen'
+import CartScreen from './app/cart/CartScreen';
+
+import AddCardScreen from './app/payment/AddCardScreen';
+import PaymentOptionScreen from './app/payment/PaymentOptionScreen';
+import PaymentScreen from './app/payment/PaymentScreen';
+import ReceiptScreen from './app/payment/ReceiptScreen';
+
+import TransactionDetailsScreen from './app/transactions/TransactionDetailsScreen';
+import TransactionListScreen from './app/transactions/TransactionListScreen';
+
+import ProfileScreen from './app/profile/ProfileScreen';
+
+import SplashScreen from './app/screens/SplashScreen';
+import ReferralScreen from './app/screens/ReferralScreen';
+import SettingsScreen from './app/screens/SettingsScreen';
+import FAQScreen from './app/screens/FAQScreen';
+
+import DrawerContent from './app/components/DrawerContent';
+
+
+var v = require('./app/stylesheets/variables');
+
+const DrawerNavigation = DrawerNavigator(
+  {
+    Home: {
+      screen: HomeScreen
+    },
+    Saved: {
+      screen: SavedScreen
+    },
+    Squads: {
+      screen: SquadScreen
+    },
+    Profile: {
+      screen: ProfileScreen
+    }
+  },
+  {
+    initialRouteName: 'Home',
+    drawerPosition: 'left',
+    contentComponent: DrawerContent,
+    contentOptions: {
+      activeTintColor: v.HASTE_COLOR,
+    }
+  }
+);
+
+const StackNavigation = StackNavigator({
+  LoginScreen: {
+    screen: LoginScreen,
+    navigationOptions: {
+      gesturesEnabled: false,
+    }
+  },
+  LoginWithEmailScreen: {
+    screen: LoginWithEmailScreen
+  },
+
+  RegisterScreen: {
+    screen: RegisterScreen
+  },
+
+  Home: {
+    screen: DrawerNavigation,
+    navigationOptions: {
+      gesturesEnabled: false,
+    }
+  },
+
+  BasketSummaryScreen: {
+    screen: BasketSummaryScreen
+  },
+
+  CartScreen: {
+    screen: CartScreen,
+    navigationOptions: {
+      gesturesEnabled: false,
+    }
+  },
+
+  PaymentOptionScreen: {
+    screen: PaymentOptionScreen,
+  },
+
+  TransactionDetailsScreen: {
+    screen: TransactionDetailsScreen,
+    navigationOptions: {
+      gesturesEnabled: false,
+    }
+  },
+
+  AddCardScreen: {
+    screen: AddCardScreen,
+  },
+
+  ReceiptScreen: {
+    screen: ReceiptScreen,
+    navigationOptions: {
+      gesturesEnabled: false,
+    }
+  }
+},
+  {
+    //initialRouteName: 'TransactionDetailsScreen',
+    headerMode: 'none'
+  }
+);
+
+export default () => <Root><StackNavigation /></Root>;
+
+/* export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -11,7 +130,7 @@ export default class App extends React.Component {
       </View>
     );
   }
-}
+} */
 
 const styles = StyleSheet.create({
   container: {
