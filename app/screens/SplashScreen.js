10b2e08c5a671983cb77c8dfc982aae8730e80a4
Myo <xxyu.2014@sis.smu.edu.sg>
2018-03-05 20:43:11 +0800

    editing splashscreen js

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Platform, Text, View, Image, TouchableOpacity, KeyboardAvoidingView, AsyncStorage, Alert, BackHandler, } from 'react-native';
 import { AppLoading, Font } from 'expo';
 import { Toast } from 'native-base';
+import { NavigationActions } from 'react-navigation';
+import LoadingIndicator from '../components/LoadingIndicator';
+
+// import { LinearGradient } from Expo;
+
+var api = require('../../api');
+var style_theme = require('../stylesheets/theme');
+var style_login = require('../stylesheets/loginScreen');
 
 export default class SplashScreen extends Component {
   constructor(props) {
@@ -9,11 +17,11 @@ export default class SplashScreen extends Component {
 
     this.state = {
       fontLoaded: false,
+      isLoading: false,
     };
   }
 
   async componentDidMount() {
-    this.getAuth();
     BackHandler.addEventListener('backPress', this.handleBackButton);
     await Font.loadAsync({
       'Roboto_medium': require('../resources/fonts/Roboto/Roboto-Medium.ttf'),
@@ -46,14 +54,24 @@ export default class SplashScreen extends Component {
       this.state.fontLoaded ? (
         <KeyboardAvoidingView behavior="padding" style={style_theme.styles.wrapper}>
 
+          {/* <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.wrapper}> */}
           <View style={style_theme.styles.logoContainer}>
             <Image
-              source={require('../resources/img/xsquad-logo.png')}
+              source={require('../resources/img/haste-logo.png')}
               style={style_theme.styles.logoSmall} />
 
             <Text style={style_theme.styles.h1}>Welcome to Haste</Text>
             <Text style={style_theme.styles.h1}>Shopping Made Easy</Text>
           </View>
+
+          <TouchableOpacity style={style_login.styles.registerRedirect}
+            onPress={this.registerRedirect}>
+            <Text style={style_theme.styles.p}>New User?</Text>
+            <Text style={style_theme.styles.bold}> Sign up here </Text>
+          </TouchableOpacity>
+
+          <LoadingIndicator isLoading={this.state.isLoading} />
+
           {/* </LinearGradient>      */}
         </KeyboardAvoidingView>
       ) : null
@@ -61,4 +79,5 @@ export default class SplashScreen extends Component {
 
 
   }
+
 }
